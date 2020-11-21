54f2c001332f519cb5738a2061b50403a043897e
vladmog <vladmog@gmail.com>
2020-11-21 13:50:38 -0800

    finished chapter 3 drill

---
+++ Goodbye cruel world/Goodbye cruel world/Goodbye cruel world.cpp
@@ -0,0 +1,19 @@
+
+// Fix the errors
+
+//#include "../../std_lib_facilities.h"
+
+//int Main()
+//{
+//	STRING s = "Goodbye, cruel world! ";
+//	cOut << S << '\n';
+//}
+
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+	string s = "Goodbye, cruel world! ";
+	cout << s << '\n';
+}
+
